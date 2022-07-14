9ac857d156468c15e78aedb35ec5fd37202dd0c9
j-lewandowski <kubalew.pl@gmail.com>
2022-07-14 15:30:57 +0200

    connected to database and added register functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/useController");
+const { registerUser, loginUser } = require("../controllers/userController");
 
 router.post("/", registerUser);
 
