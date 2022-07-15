3cf2a096c3b96bf532185a94487e3226029adc90
j-lewandowski <kubalew.pl@gmail.com>
2022-07-15 15:36:04 +0200

    added tickets listing, single ticket display and closing ticket functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 function Home() {
   return (
     <>
-      <section class='heading'>
+      <section className='heading'>
         <h1> What do you need help with?</h1>
         <p>Please choose from an option below</p>
       </section>
